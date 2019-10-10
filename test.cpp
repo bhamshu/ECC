1ee1ed40af4a3f1170012b51e3f77de881deaa84
bhamshu <4dman.com@gmail.com>
2019-10-10 14:47:48 +0530

    Added many more tests for elliptic curve group operations

--- test.cpp
+++ test.cpp
@@ -28,12 +28,29 @@ int main(){
 
 	EC ec(1, 6, p11);
 	EllipticCurve curve(ec);
+	
 	EcElement p0 = curve.getEcPoint(2, 4);
 	EcElement p1(3, 5, ec);
 	EcElement p2 = p0 + p1;
+	assert(p2.isEqual(curve.getEcPoint(7, 2)));
 	show(p0); std::cout<<" * ";
 	show(p1); std::cout<<" = ";
 	show(p2);
 	std::cout<<"  [y^2 = x^3+x+6 (mod 11)]\n";
+	
+	//Following test examples taken from https://youtu.be/vnpZXJL6QCQ?t=4713
+	EC ec2(2, 2, 17);
+	curve = ec2;
+	assert( curve.getEcPoint(6, 3).isEqual(curve.getEcPoint(5, 1)+curve.getEcPoint(5, 1)) );
+	assert( curve.getEcPoint(10, 6).isEqual(curve.getEcPoint(6, 3)+curve.getEcPoint(5, 1)) );
+	assert( curve.getEcPoint(3, 1).isEqual(curve.getEcPoint(10, 6)+curve.getEcPoint(5, 1)) );
+	assert( curve.getEcPoint(3, 1).isEqual(curve.getEcPoint(6, 3)+curve.getEcPoint(6, 3)) );
+	assert( curve.getEcPoint(9, 16).isEqual(curve.getEcPoint(6, 3)+curve.getEcPoint(10, 6)) );
+	assert( curve.getEcPoint(7, 11).isEqual(curve.getEcPoint(10, 6)+curve.getEcPoint(0, 6)) );
+	assert( curve.getEcPoint(-1, -1).isEqual(curve.getEcPoint(5, 1)+curve.getEcPoint(5, 16)) );
+	assert( curve.getEcPoint(-1, -1).isEqual(curve.getEcPoint(0, 6)+curve.getEcPoint(0, 11)) );
+	assert( curve.getEcPoint(5, 1).isEqual(curve.getEcPoint(5, 1)+curve.getEcPoint(-1, -1)) );
+	/****************************************************************************************/
+
 	return 0;
 }
